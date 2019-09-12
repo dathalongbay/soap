b10084232f41b85ae9b5de8bf6f29d101dc78bbe
dathalong710 <dathalongbay@gmail.com>
2019-09-12 22:43:49 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -19,6 +19,6 @@
         array("return" => "xsd:string")
     );
     
-    $HTTP_RAW_POST_DATA = isset($HTTP_RAW_POST_DATA) ? $HTTP_RAW_POST_DATA : "";
-    $server->service($HTTP_RAW_POST_DATA);
+    //$HTTP_RAW_POST_DATA = isset($HTTP_RAW_POST_DATA) ? $HTTP_RAW_POST_DATA : "";
+    $server->service(file_get_contents("php://input"));
     
