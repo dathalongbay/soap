8ec547ec9093b360f52275814b419ba0e415ab1c
dathalong710 <dathalongbay@gmail.com>
2019-09-12 22:15:24 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+    require "lib/nusoap.php";
+    require "functions.php";
+    
+    $server = new nusoap_server();
+    $server->configureWSDL("Partner", "urn:Partner");
+    
+    $server->register(
+        "ReceiveResponse",
+        array(
+            "MOId"          => "xsd:int",
+            "Telco"         => "xsd:string",
+            "ServiceNum"    => "xsd:string",
+            "Phone"         => "xsd:string",
+            "Syntax"        => "xsd:string",
+            "EncryptedMessage" => "xsd:string",
+            "Signature"     => "xsd:string"
+        ),
+        array("return" => "xsd:string")
+    );
+    
+    $HTTP_RAW_POST_DATA = isset($HTTP_RAW_POST_DATA) ? $HTTP_RAW_POST_DATA : "";
+    $server->service($HTTP_RAW_POST_DATA);
+    
